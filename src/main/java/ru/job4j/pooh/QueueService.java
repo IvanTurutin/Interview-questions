af1451a05a2f4c5c2899d625839f5fec12f96a70
Ivan <95500385+IvanTurutin@users.noreply.github.com>
2022-10-27 16:52:34 +0300

    Update QueueService.java

--- src/main/java/ru/job4j/pooh/QueueService.java
+++ src/main/java/ru/job4j/pooh/QueueService.java
@@ -1,8 +1,90 @@
 package ru.job4j.pooh;
+import java.util.Map;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.Queue;
 
 public class QueueService implements Service {
-    @Override
+	private Map<String, ConcurrentLinkedQueue<String>> queue = new ConcurrentHashMap<>();
+	private static final String REQUEST_DONE = "200";
+	private static final String REQUEST_NO_DATA = "204";
+    
+	@Override
     public Resp process(Req req) {
-        return null;
+        Resp rslt = null;
+		if ("POST".equals(req.httpRequestType())) {
+			Queue<String> tmpQueue = queue.putIfAbsent(req.getSourceName(), new ConcurrentLinkedQueue<>());
+			if (tmpQueue == null) {
+				tmpQueue = queue.get(req.getSourceName());
+			}
+			tmpQueue.add(req.getParam());
+			rslt = new Resp(req.getParam(), REQUEST_DONE);
+		} else {
+			Queue<String> tmpQueue = queue.get(req.getSourceName());
+			if (tmpQueue == null) {
+				rslt = new Resp("", REQUEST_NO_DATA);
+			} else {
+				String param = tmpQueue.poll();
+				if (param == null) {
+					rslt = new Resp("", REQUEST_NO_DATA);
+				} else {
+					rslt = new Resp(param, REQUEST_DONE);
+				}
+			}
+		}
+		return rslt;
+    }
+	
+	/*1 - whenPostThenGetQueue*/
+/*	public static void main(String[] args) {
+        QueueService queueService = new QueueService();
+        String paramForPostMethod = "temperature=18";
+        // Добавляем данные в очередь weather. Режим queue 
+        queueService.process(
+                new Req("POST", "queue", "weather", paramForPostMethod)
+        );
+        // Забираем данные из очереди weather. Режим queue
+        Resp result = queueService.process(
+                new Req("GET", "queue", "weather", null)
+        );
+        System.out.println("temperature=18".equals(result.text()));
+		System.out.println("200".equals(result.status()));
+
+    }
+	*/
+
+	/*2 - whenPostThenGetQueueTwice*/
+/*	public static void main(String[] args) {
+        QueueService queueService = new QueueService();
+        String paramForPostMethod = "temperature=18";
+        //Добавляем данные в очередь weather. Режим queue
+        queueService.process(
+                new Req("POST", "queue", "weather", paramForPostMethod)
+        );
+        // Забираем данные из очереди weather. Режим queue
+        Resp result = queueService.process(
+                new Req("GET", "queue", "weather", null)
+        );
+		result = queueService.process(
+                new Req("GET", "queue", "weather", null)
+        );
+        System.out.println("".equals(result.text()));
+		System.out.println("204".equals(result.status()));
+    }
+*/
+	/*3 - whenPostThenGetMissingQueue*/
+	public static void main(String[] args) {
+        QueueService queueService = new QueueService();
+        String paramForPostMethod = "temperature=18";
+        //Добавляем данные в очередь weather. Режим queue
+        queueService.process(
+                new Req("POST", "queue", "weather", paramForPostMethod)
+        );
+        // Забираем данные из очереди weather. Режим queue
+        Resp result = queueService.process(
+                new Req("GET", "queue", "news", null)
+        );
+        System.out.println("".equals(result.text()));
+		System.out.println("204".equals(result.status()));
     }
 }
